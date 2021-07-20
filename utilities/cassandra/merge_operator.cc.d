eb6c94c2fe3865b53ebbdbed194eb581a0c2c87d
inhoinno <mearrong123@gmail.com>
2021-07-20 22:23:44 +0900

    KV distribution

--- utilities/cassandra/merge_operator.cc.d
+++
@@ -1,8 +0,0 @@
-utilities/cassandra/merge_operator.cc.d \
- utilities/cassandra/merge_operator.o \
- utilities/cassandra/merge_operator.o: \
- utilities/cassandra/merge_operator.cc \
- utilities/cassandra/merge_operator.h include/rocksdb/merge_operator.h \
- include/rocksdb/slice.h include/rocksdb/cleanable.h \
- include/rocksdb/rocksdb_namespace.h utilities/merge_operators.h \
- utilities/cassandra/format.h
